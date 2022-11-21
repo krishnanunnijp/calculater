1c3aa516dd86f387e382ea09e8898a757f928ba1
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-21 10:16:36 +0530

    index updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1.addition");
+        System.out.println("2.subtraction");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
     }
 }
