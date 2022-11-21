d8e7da9aea2802cbbf9e2654e13cb6ff26fa12ce
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-21 10:27:18 +0530

    index added

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,7 @@ public class Main {
         int input= sc.nextInt();
         int input1= sc.nextInt();
         int input2 = sc.nextInt();
-        switch (input){
-            case 1:
-                System.out.println(input1+input2);
-                break;
+
 
 
         }
